5154c5071bdb616b640f7e085e4de362762825ac
Marcin Tatarski <martin.tatarski@gmail.com>
2025-08-13 17:13:12 +0100

    Add Add settings model and configuration services. Create GET request to get top level tasklists Id's.

---
+++ Services/ConfigurationManager.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using TeamworkWeeklyReport.Models;
+
+namespace TeamworkWeeklyReport.Services
+{
+    // Non static ConfigManager because a static class cannot have any instance members.
+    public class ConfigManager
+    {
+        // Instead I use static Constructor and static memebers for more future flexibliity
+        private static readonly IConfigurationRoot _configuration;
+        private static readonly AppSettings _appSettings;
+
+        static ConfigManager()
+        {
+            // Determinate base path
+            var devRootPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", ".."));
+
+            var jsonSettingsPath = Path.Combine(devRootPath, "appsettings.Development.json");
+
+            string basePath;
+
+            if (File.Exists(jsonSettingsPath)) { 
+                basePath = devRootPath;
+            }
+            else
+            {
+                basePath = AppContext.BaseDirectory;
+            }
+
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+            
+            _appSettings = _configuration.Get<AppSettings>();           
+        }
+
+        public static AppSettings Settings => _appSettings;
+
+        public string GetSettings(string key)
+        {
+            return _configuration[key];
+        }
+    }
+}
